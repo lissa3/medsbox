e690794204a547c6b713c67bd6726ddb7605cc3f
tata <tspan2017@gmail.com>
2023-07-14 22:59:33 +0200

    For #4 set admin for User model

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .forms import UserCreationForm
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """based on AbstractUser model;"""
+
+    search_fields = ("email", "username")
+    add_form = UserCreationForm
+    list_display = (
+        "id",
+        "email",
+        "last_login",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+    )
+    readonly_fields = ["last_login", "date_joined"]
+    list_filter = ("is_active", "is_superuser")
+    add_fieldsets = (
+        (
+            ("Add Your User"),
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
