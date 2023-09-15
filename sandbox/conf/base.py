852e6062831d65a486fc97be990da36b6e72704e
tata <tspan2017@gmail.com>
2023-09-16 01:24:33 +0200

    add Comment model and htmx func for crud

--- sandbox/conf/base.py
+++ sandbox/conf/base.py
@@ -93,16 +93,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "sandbox.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PSW"),
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": env("DB_NAME"),
+#         "USER": env("DB_USER"),
+#         "PASSWORD": env("DB_PSW"),
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 # DATABASES = {
 #     "default": {
