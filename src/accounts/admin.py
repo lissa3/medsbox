559860b085e1452dba1e08f3cd56df493ae4e591
tata <tspan2017@gmail.com>
2023-10-01 16:57:06 +0200

    For #4 refactor profile admin; change user form in admin; test

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm
 
 from .forms import UserCreationForm
 
@@ -13,14 +14,18 @@ class UserAdmin(BaseUserAdmin):
 
     search_fields = ("email", "username")
     add_form = UserCreationForm
+    form = UserChangeForm
     list_display = (
-        "id",
+        "username",
+        "banned",
         "email",
         "last_login",
         "is_staff",
         "is_active",
         "is_superuser",
+        "id",
     )
+    list_display_links = ["username", "email"]
     readonly_fields = ["last_login", "date_joined"]
     list_filter = ("is_active", "is_superuser")
     add_fieldsets = (
@@ -29,6 +34,21 @@ class UserAdmin(BaseUserAdmin):
             {
                 "classes": ("wide",),
                 "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+    fieldsets = (
+        (
+            ("Main"),
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "banned",
+                    "deactivated_on",
+                    "blackListEmail",
+                ),
             },
         ),
     )
--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -3,7 +3,6 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm as UCF
 from django.utils.html import format_html
-from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
