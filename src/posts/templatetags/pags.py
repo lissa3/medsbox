4df930146aaf79da66bbcc0bdb706876f7eedab8
tata <tspan2017@gmail.com>
2023-10-15 23:00:35 +0200

    For #68 :recycle: add pagination(draft) for htmx search categs,tags,calend

---
+++ src/posts/templatetags/pags.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag("components/htmx_pags.html")
+def pags_tag(tag=None, slug=None, page_obj=None, request=None, year=None, month=None):
+    """pagination for tags search"""
+    ctx = {"page_obj": page_obj, "req_url": request.path}
+    if tag:
+        ctx.update({"tag": tag})
+    if slug:
+        ctx.update({"slug": slug})
+    if year and month:
+        {ctx.update({"year": year, "month": month})}
+
+    return ctx
